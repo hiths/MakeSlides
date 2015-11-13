845933f7d987e0ee5c516ec70cd048dacd56b059
xuhuan <xuhuanxxx@gmail.com>
2015-11-13 19:15:53 +0800

    1113

--- TablePresentation/Program.cs
+++ TablePresentation/Program.cs
@@ -1,43 +1,31 @@
 ﻿using System;
-using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using ExcelManipulater;
+using System.IO;
 
-namespace FetchCell
+namespace TestForExcelManipulater
 {
-    public class Cell
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            object Nothing = System.Reflection.Missing.Value;
-            var xlApp = new Excel.Application();
-            xlApp.Visible = true;
-            Excel.Workbook workBook = xlApp.Workbooks.Add(Nothing);
-            Excel.Worksheet workSheet = (Excel.Worksheet)workBook.Sheets[1];
-            Excel.Range range = workSheet.UsedRange;
-            workSheet.Name = "sheet";
-            workSheet.Cells[1, 1] = "FileName";
-            workSheet.Cells[1, 2] = "FindString";
-            workSheet.Cells[1, 3] = "ReplaceString";
-            Excel.Range thisCell = range.Cells[1,1];
-            Object value = thisCell.Value;
-            //fontcolor
-            double fontColor = thisCell.Font.Color;
-            //background color
-            double bgColor = thisCell.Interior.Color;
-            //cell note
-            //string note = "sb";
-            //thisCell.Comment.Text = note;
-            var thisArray = new []{ value, fontColor, bgColor };
-            foreach(var i in thisArray)
+            string fileName = System.Environment.CurrentDirectory + "\\a.xlsx";
+            Console.WriteLine(fileName);
+            DataSet sheets = ExcelReader.ImportDataFromAllSheets(fileName);
+            if (sheets != null)
             {
-                Console.WriteLine(i);
-            };
-            Console.ReadKey();
-            Console.WriteLine(thisCell.Comment.Text());
+                foreach (DataTable dt in sheets.Tables)
+                {
+                    Console.WriteLine(dt.TableName.ToString());
+                }
+                //ExcelWriter.ExportDataToExcel(sheets, "copy.xls");
+            }
 
-            //workSheet.SaveAs("1.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing);
-            //workBook.Close(false, Type.Missing, Type.Missing);
-            //xlApp.Quit();
+            Console.WriteLine("Finish");
+            Console.ReadKey();
         }
     }
-
 }
